40d3a0dce3f132313b37807698ecb30bcc0a39a7
faithebear <149540831+faithebear@users.noreply.github.com>
2025-02-04 11:58:33 -0700

    save sql

--- models/visual_editor/Untitled.sql
+++ models/visual_editor/Untitled.sql
@@ -2,17 +2,10 @@ WITH customers AS (
   SELECT
     *
   FROM {{ ref('customers') }}
-), filter_1 AS (
-  SELECT
-    *
-  FROM customers
-  WHERE
-    CUSTOMER_NAME = 'Stephanie'
 ), aggregation_1 AS (
   SELECT
-    COUNT(CUSTOMER_ID) AS count_CUSTOMER_ID,
-    MAX(LIFETIME_TAX_PAID) AS max_LIFETIME_TAX_PAID
-  FROM filter_1
+    COUNT(DISTINCT CUSTOMER_ID) AS countd_CUSTOMER_ID
+  FROM customers
 ), untitled_sql AS (
   SELECT
     *
