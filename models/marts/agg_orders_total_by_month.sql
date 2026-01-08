935626bb0ad04a8bbca91e9f051a5aca07763e07
maguire marion <maguire.marion@dbtlabsworkshop.com>
2026-01-08 23:38:41 +0000

    incremental

--- models/marts/agg_orders_total_by_month.sql
+++ models/marts/agg_orders_total_by_month.sql
@@ -3,6 +3,10 @@ SELECT
     SUM(order_total) AS order_total_sum
 FROM 
     {{ ref('orders') }}
+
+    {% if is_incremental() %}
+        where order_month > (select max(order_month) from {{ this }}) 
+    {% endif %}
 GROUP BY 
     1
 ORDER BY 
